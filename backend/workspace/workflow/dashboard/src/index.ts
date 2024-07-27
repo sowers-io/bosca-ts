9039f41eedf599017cdd7ff5b84b826ae2b70739
Kyle Brock <brock@donesky.io>
2024-07-27 09:27:02 -0500

    updates

---
+++ backend/workspace/workflow/dashboard/src/index.ts
@@ -0,0 +1,49 @@
+import { createBullBoard } from "@bull-board/api";
+import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
+import { FastifyAdapter } from "@bull-board/fastify";
+import { Queue as QueueMQ } from "bullmq";
+import fastify from "fastify";
+import { logger } from "@bosca/common";
+
+async function main() {
+  const queueNames = [
+    "metadata",
+    "traits",
+    "transition",
+    "bible",
+    "bible-verse",
+    "bible-book",
+    "bible-ai",
+    "search-index",
+  ];
+  const app = fastify({
+    logger: {
+      level: "debug",
+    },
+  });
+  const serverAdapter = new FastifyAdapter();
+  createBullBoard({
+    queues: queueNames.map(
+      (name) =>
+        new BullMQAdapter(
+          new QueueMQ(name, {
+            connection: {
+              port: 6379,
+              host: "localhost",
+            },
+          })
+        )
+    ),
+    serverAdapter,
+  });
+  serverAdapter.setBasePath("/ui");
+  app.register(serverAdapter.registerPlugin(), { basePath: "/ui" });
+
+  await app.listen({ port: 3000 });
+  logger.info("For the UI, open http://localhost:3000/ui");
+}
+
+main().catch((e) => {
+  logger.error(e);
+  process.exit(1);
+});

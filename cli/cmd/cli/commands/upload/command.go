2e5e65d29243cbc260dc5d3ee1376b7bb103076b
Kyle Brock <kyle@sowers.io>
2024-08-29 15:03:57 -0500

    working on compose

--- cli/cmd/cli/commands/upload/command.go
+++ cli/cmd/cli/commands/upload/command.go
@@ -17,13 +17,16 @@
 package upload
 
 import (
+	"context"
+	"errors"
+	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
 
+	"bosca.io/api/protobuf/bosca"
+	"bosca.io/api/protobuf/bosca/content"
 	"bosca.io/cmd/cli/commands/flags"
-	"bosca.io/cmd/cli/commands/login"
-	"github.com/eventials/go-tus"
+	"bosca.io/pkg/cli"
 	"github.com/spf13/cobra"
 )
 
@@ -37,48 +40,60 @@ var Command = &cobra.Command{
 			panic(err)
 		}
 		defer f.Close()
-
 		endpoint := cmd.Flag(flags.EndpointFlag).Value.String()
-
 		cmd.Printf("Uploading to %v", endpoint)
-
-		cfg := tus.DefaultConfig()
-
-		session, err := login.GetSession()
+		client, err := cli.NewContentClient(cmd)
+		if err != nil {
+			return err
+		}
+		ctx, err := cli.GetAuthenticatedContext(context.Background())
+		if err != nil {
+			return err
+		}
+		s, _ := f.Stat()
+		size := s.Size()
+		metadata, err := client.AddMetadata(ctx, &content.AddMetadataRequest{
+			Metadata: &content.Metadata{
+				Name:          args[0],
+				ContentType:   "application/octet-stream",
+				ContentLength: &size,
+				LanguageTag:   "en",
+			},
+		})
+		if err != nil {
+			return err
+		}
+		signedUrl, err := client.GetMetadataUploadUrl(ctx, &bosca.IdRequest{
+			Id: metadata.Id,
+		})
 		if err != nil {
 			return err
 		}
 
-		cfg.Header.Set("Authorization", "Bearer "+session)
+		cmd.Printf("\r\n\r\n%v\r\n\r\n", signedUrl)
 
-		client, err := tus.NewClient(endpoint, cfg)
+		// req, err := http.NewRequest(signedUrl.Method, strings.Replace(signedUrl.Url, "bosca.minio", "minio", 1), f)
+		req, err := http.NewRequest(signedUrl.Method, signedUrl.Url, f)
+		for _, h := range signedUrl.Headers {
+			req.Header.Add(h.Name, h.Value)
+		}
 		if err != nil {
 			return err
 		}
-		upload, err := tus.NewUploadFromFile(f)
+		res, err := http.DefaultClient.Do(req)
 		if err != nil {
 			return err
 		}
-		buf := make([]byte, 0, 8096)
-		f.Read(buf)
-		f.Seek(0, 0)
-
-		parts := strings.Split(f.Name(), string(os.PathSeparator))
-		if len(args) == 2 {
-			upload.Metadata["id"] = args[1]
+		if res.StatusCode != 200 {
+			s, _ := ioutil.ReadAll(res.Body)
+			return errors.New(res.Status + ": " + string(s))
 		}
-		upload.Metadata["name"] = parts[len(parts)-1]
-		upload.Metadata["filetype"] = http.DetectContentType(buf)
-		upload.Metadata["traits"] = cmd.Flag(flags.TraitFlag).Value.String()
-		uploader, err := client.CreateUpload(upload)
-		if err != nil {
-			return err
-		}
-		return uploader.Upload()
+		_, err = client.SetMetadataReady(ctx, &content.MetadataReadyRequest{Id: metadata.Id})
+		return err
 	},
 }
 
 func init() {
 	Command.PersistentFlags().String(flags.TraitFlag, "", "Trait ID")
-	Command.PersistentFlags().String(flags.EndpointFlag, "http://localhost:7001/files", "The endpoint to use.")
+	Command.PersistentFlags().String(flags.EndpointFlag, "localhost:7000", "The endpoint to use.")
 }

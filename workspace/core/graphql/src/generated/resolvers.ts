29721261e9c76961ce6b6fb3ce7103292bc05166
Kyle Brock <kyle@sowers.io>
2024-09-01 00:47:33 -0500

    signup

--- workspace/core/graphql/src/generated/resolvers.ts
+++ workspace/core/graphql/src/generated/resolvers.ts
@@ -388,7 +388,6 @@ export interface MutationSignupArgs {
   firstName: Scalars['String']['input'];
   lastName: Scalars['String']['input'];
   password: Scalars['String']['input'];
-  username: Scalars['String']['input'];
 }
 
 export interface Permission {
@@ -1016,7 +1015,7 @@ export type MutationResolvers<ContextType = any, ParentType extends ResolversPar
   setMetadataReady?: Resolver<Maybe<ResolversTypes['Metadata']>, ParentType, ContextType, RequireFields<MutationSetMetadataReadyArgs, 'id'>>;
   setMetadataTextContent?: Resolver<Maybe<ResolversTypes['Metadata']>, ParentType, ContextType, RequireFields<MutationSetMetadataTextContentArgs, 'id'>>;
   setPassword?: Resolver<Maybe<ResolversTypes['Boolean']>, ParentType, ContextType, RequireFields<MutationSetPasswordArgs, 'password'>>;
-  signup?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType, RequireFields<MutationSignupArgs, 'email' | 'firstName' | 'lastName' | 'password' | 'username'>>;
+  signup?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType, RequireFields<MutationSignupArgs, 'email' | 'firstName' | 'lastName' | 'password'>>;
 }>;
 
 export type PermissionResolvers<ContextType = any, ParentType extends ResolversParentTypes['Permission'] = ResolversParentTypes['Permission']> = ResolversObject<{
